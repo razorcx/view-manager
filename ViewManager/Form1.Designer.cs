d9fe2eec5b5559ce9cbe318d7e5e6470afff3ac8
Cory LePage <cory@razorcx.com>
2017-12-21 13:41:23 -0800

    Initial commit

--- ViewManager/Form1.Designer.cs
+++ ViewManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ViewManager
 {
-	partial class Form1
+	partial class ViewManager
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,274 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewManager));
+			this.buttonControlLine = new System.Windows.Forms.Button();
+			this.buttonOff = new System.Windows.Forms.Button();
+			this.buttonOn = new System.Windows.Forms.Button();
+			this.buttonControlPoint = new System.Windows.Forms.Button();
+			this.buttonBaseComponent = new System.Windows.Forms.Button();
+			this.buttonBaseWeld = new System.Windows.Forms.Button();
+			this.buttonPart = new System.Windows.Forms.Button();
+			this.buttonBoltGroup = new System.Windows.Forms.Button();
+			this.buttonCutFitting = new System.Windows.Forms.Button();
+			this.listBoxViews = new System.Windows.Forms.ListBox();
+			this.buttonDeleteView = new System.Windows.Forms.Button();
+			this.buttonCloseView = new System.Windows.Forms.Button();
+			this.buttonOpenView = new System.Windows.Forms.Button();
+			this.groupBoxQuickOnOff = new System.Windows.Forms.GroupBox();
+			this.groupBoxOnOff = new System.Windows.Forms.GroupBox();
+			this.groupBoxViews = new System.Windows.Forms.GroupBox();
+			this.buttonCloseAllViews = new System.Windows.Forms.Button();
+			this.buttonCloseCurrentView = new System.Windows.Forms.Button();
+			this.buttonAllOff = new System.Windows.Forms.Button();
+			this.groupBoxQuickOnOff.SuspendLayout();
+			this.groupBoxOnOff.SuspendLayout();
+			this.groupBoxViews.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// buttonControlLine
+			// 
+			this.buttonControlLine.Location = new System.Drawing.Point(6, 31);
+			this.buttonControlLine.Name = "buttonControlLine";
+			this.buttonControlLine.Size = new System.Drawing.Size(110, 46);
+			this.buttonControlLine.TabIndex = 0;
+			this.buttonControlLine.Text = "Control Line";
+			this.buttonControlLine.UseVisualStyleBackColor = true;
+			this.buttonControlLine.Click += new System.EventHandler(this.buttonControlLines_Click);
+			// 
+			// buttonOff
+			// 
+			this.buttonOff.Location = new System.Drawing.Point(238, 21);
+			this.buttonOff.Name = "buttonOff";
+			this.buttonOff.Size = new System.Drawing.Size(111, 44);
+			this.buttonOff.TabIndex = 1;
+			this.buttonOff.Text = "Single OFF";
+			this.buttonOff.UseVisualStyleBackColor = true;
+			this.buttonOff.Click += new System.EventHandler(this.buttonOff_Click);
+			// 
+			// buttonOn
+			// 
+			this.buttonOn.Location = new System.Drawing.Point(7, 21);
+			this.buttonOn.Name = "buttonOn";
+			this.buttonOn.Size = new System.Drawing.Size(109, 44);
+			this.buttonOn.TabIndex = 2;
+			this.buttonOn.Text = "All ON";
+			this.buttonOn.UseVisualStyleBackColor = true;
+			this.buttonOn.Click += new System.EventHandler(this.buttonOn_Click);
+			// 
+			// buttonControlPoint
+			// 
+			this.buttonControlPoint.Location = new System.Drawing.Point(6, 83);
+			this.buttonControlPoint.Name = "buttonControlPoint";
+			this.buttonControlPoint.Size = new System.Drawing.Size(110, 46);
+			this.buttonControlPoint.TabIndex = 3;
+			this.buttonControlPoint.Text = "Control Point";
+			this.buttonControlPoint.UseVisualStyleBackColor = true;
+			this.buttonControlPoint.Click += new System.EventHandler(this.buttonControlPoint_Click);
+			// 
+			// buttonBaseComponent
+			// 
+			this.buttonBaseComponent.Location = new System.Drawing.Point(122, 83);
+			this.buttonBaseComponent.Name = "buttonBaseComponent";
+			this.buttonBaseComponent.Size = new System.Drawing.Size(110, 46);
+			this.buttonBaseComponent.TabIndex = 4;
+			this.buttonBaseComponent.Text = "Base Component";
+			this.buttonBaseComponent.UseVisualStyleBackColor = true;
+			this.buttonBaseComponent.Click += new System.EventHandler(this.buttonBaseComponent_Click);
+			// 
+			// buttonBaseWeld
+			// 
+			this.buttonBaseWeld.Location = new System.Drawing.Point(122, 31);
+			this.buttonBaseWeld.Name = "buttonBaseWeld";
+			this.buttonBaseWeld.Size = new System.Drawing.Size(110, 46);
+			this.buttonBaseWeld.TabIndex = 5;
+			this.buttonBaseWeld.Text = "Base Weld";
+			this.buttonBaseWeld.UseVisualStyleBackColor = true;
+			this.buttonBaseWeld.Click += new System.EventHandler(this.buttonBaseWeld_Click);
+			// 
+			// buttonPart
+			// 
+			this.buttonPart.Location = new System.Drawing.Point(238, 31);
+			this.buttonPart.Name = "buttonPart";
+			this.buttonPart.Size = new System.Drawing.Size(110, 46);
+			this.buttonPart.TabIndex = 6;
+			this.buttonPart.Text = "Part";
+			this.buttonPart.UseVisualStyleBackColor = true;
+			this.buttonPart.Click += new System.EventHandler(this.buttonPart_Click);
+			// 
+			// buttonBoltGroup
+			// 
+			this.buttonBoltGroup.Location = new System.Drawing.Point(238, 83);
+			this.buttonBoltGroup.Name = "buttonBoltGroup";
+			this.buttonBoltGroup.Size = new System.Drawing.Size(110, 46);
+			this.buttonBoltGroup.TabIndex = 7;
+			this.buttonBoltGroup.Text = "Bolt Group";
+			this.buttonBoltGroup.UseVisualStyleBackColor = true;
+			this.buttonBoltGroup.Click += new System.EventHandler(this.buttonBoltGroup_Click);
+			// 
+			// buttonCutFitting
+			// 
+			this.buttonCutFitting.Location = new System.Drawing.Point(122, 135);
+			this.buttonCutFitting.Name = "buttonCutFitting";
+			this.buttonCutFitting.Size = new System.Drawing.Size(110, 46);
+			this.buttonCutFitting.TabIndex = 8;
+			this.buttonCutFitting.Text = "Cut/Fitting";
+			this.buttonCutFitting.UseVisualStyleBackColor = true;
+			this.buttonCutFitting.Click += new System.EventHandler(this.buttonCutFitting_Click);
+			// 
+			// listBoxViews
+			// 
+			this.listBoxViews.FormattingEnabled = true;
+			this.listBoxViews.ItemHeight = 16;
+			this.listBoxViews.Location = new System.Drawing.Point(6, 21);
+			this.listBoxViews.Name = "listBoxViews";
+			this.listBoxViews.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.listBoxViews.Size = new System.Drawing.Size(341, 228);
+			this.listBoxViews.TabIndex = 9;
+			// 
+			// buttonDeleteView
+			// 
+			this.buttonDeleteView.Location = new System.Drawing.Point(238, 255);
+			this.buttonDeleteView.Name = "buttonDeleteView";
+			this.buttonDeleteView.Size = new System.Drawing.Size(110, 46);
+			this.buttonDeleteView.TabIndex = 12;
+			this.buttonDeleteView.Text = "Delete";
+			this.buttonDeleteView.UseVisualStyleBackColor = true;
+			this.buttonDeleteView.Click += new System.EventHandler(this.buttonDeleteView_Click);
+			// 
+			// buttonCloseView
+			// 
+			this.buttonCloseView.Location = new System.Drawing.Point(121, 255);
+			this.buttonCloseView.Name = "buttonCloseView";
+			this.buttonCloseView.Size = new System.Drawing.Size(110, 46);
+			this.buttonCloseView.TabIndex = 11;
+			this.buttonCloseView.Text = "Close";
+			this.buttonCloseView.UseVisualStyleBackColor = true;
+			this.buttonCloseView.Click += new System.EventHandler(this.buttonCloseView_Click);
+			// 
+			// buttonOpenView
+			// 
+			this.buttonOpenView.Location = new System.Drawing.Point(5, 255);
+			this.buttonOpenView.Name = "buttonOpenView";
+			this.buttonOpenView.Size = new System.Drawing.Size(110, 46);
+			this.buttonOpenView.TabIndex = 10;
+			this.buttonOpenView.Text = "Open";
+			this.buttonOpenView.UseVisualStyleBackColor = true;
+			this.buttonOpenView.Click += new System.EventHandler(this.buttonOpenView_Click);
+			// 
+			// groupBoxQuickOnOff
+			// 
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonBaseWeld);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonControlLine);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonControlPoint);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonBaseComponent);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonPart);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonBoltGroup);
+			this.groupBoxQuickOnOff.Controls.Add(this.buttonCutFitting);
+			this.groupBoxQuickOnOff.Location = new System.Drawing.Point(11, 12);
+			this.groupBoxQuickOnOff.Name = "groupBoxQuickOnOff";
+			this.groupBoxQuickOnOff.Size = new System.Drawing.Size(356, 192);
+			this.groupBoxQuickOnOff.TabIndex = 14;
+			this.groupBoxQuickOnOff.TabStop = false;
+			this.groupBoxQuickOnOff.Text = "Quick On/Off";
+			// 
+			// groupBoxOnOff
+			// 
+			this.groupBoxOnOff.Controls.Add(this.buttonAllOff);
+			this.groupBoxOnOff.Controls.Add(this.buttonOff);
+			this.groupBoxOnOff.Controls.Add(this.buttonOn);
+			this.groupBoxOnOff.Location = new System.Drawing.Point(11, 210);
+			this.groupBoxOnOff.Name = "groupBoxOnOff";
+			this.groupBoxOnOff.Size = new System.Drawing.Size(357, 79);
+			this.groupBoxOnOff.TabIndex = 15;
+			this.groupBoxOnOff.TabStop = false;
+			this.groupBoxOnOff.Text = "On/Off";
+			// 
+			// groupBoxViews
+			// 
+			this.groupBoxViews.Controls.Add(this.buttonCloseAllViews);
+			this.groupBoxViews.Controls.Add(this.buttonCloseCurrentView);
+			this.groupBoxViews.Controls.Add(this.listBoxViews);
+			this.groupBoxViews.Controls.Add(this.buttonOpenView);
+			this.groupBoxViews.Controls.Add(this.buttonCloseView);
+			this.groupBoxViews.Controls.Add(this.buttonDeleteView);
+			this.groupBoxViews.Location = new System.Drawing.Point(11, 295);
+			this.groupBoxViews.Name = "groupBoxViews";
+			this.groupBoxViews.Size = new System.Drawing.Size(357, 365);
+			this.groupBoxViews.TabIndex = 16;
+			this.groupBoxViews.TabStop = false;
+			this.groupBoxViews.Text = "Views";
+			// 
+			// buttonCloseAllViews
+			// 
+			this.buttonCloseAllViews.Location = new System.Drawing.Point(177, 307);
+			this.buttonCloseAllViews.Name = "buttonCloseAllViews";
+			this.buttonCloseAllViews.Size = new System.Drawing.Size(171, 46);
+			this.buttonCloseAllViews.TabIndex = 14;
+			this.buttonCloseAllViews.Text = "Close All Views";
+			this.buttonCloseAllViews.UseVisualStyleBackColor = true;
+			this.buttonCloseAllViews.Click += new System.EventHandler(this.buttonCloseAllViews_Click);
+			// 
+			// buttonCloseCurrentView
+			// 
+			this.buttonCloseCurrentView.Location = new System.Drawing.Point(5, 307);
+			this.buttonCloseCurrentView.Name = "buttonCloseCurrentView";
+			this.buttonCloseCurrentView.Size = new System.Drawing.Size(166, 46);
+			this.buttonCloseCurrentView.TabIndex = 13;
+			this.buttonCloseCurrentView.Text = "Close Selected Views";
+			this.buttonCloseCurrentView.UseVisualStyleBackColor = true;
+			this.buttonCloseCurrentView.Click += new System.EventHandler(this.buttonCloseSelectedViews_Click);
+			// 
+			// buttonAllOff
+			// 
+			this.buttonAllOff.Location = new System.Drawing.Point(123, 21);
+			this.buttonAllOff.Name = "buttonAllOff";
+			this.buttonAllOff.Size = new System.Drawing.Size(109, 44);
+			this.buttonAllOff.TabIndex = 3;
+			this.buttonAllOff.Text = "All OFF";
+			this.buttonAllOff.UseVisualStyleBackColor = true;
+			this.buttonAllOff.Click += new System.EventHandler(this.buttonAllOff_Click);
+			// 
+			// ViewManager
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(377, 674);
+			this.Controls.Add(this.groupBoxViews);
+			this.Controls.Add(this.groupBoxOnOff);
+			this.Controls.Add(this.groupBoxQuickOnOff);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ViewManager";
+			this.Text = "View Manager";
+			this.TopMost = true;
+			this.groupBoxQuickOnOff.ResumeLayout(false);
+			this.groupBoxOnOff.ResumeLayout(false);
+			this.groupBoxViews.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button buttonControlLine;
+		private System.Windows.Forms.Button buttonOff;
+		private System.Windows.Forms.Button buttonOn;
+		private System.Windows.Forms.Button buttonControlPoint;
+		private System.Windows.Forms.Button buttonBaseComponent;
+		private System.Windows.Forms.Button buttonBaseWeld;
+		private System.Windows.Forms.Button buttonPart;
+		private System.Windows.Forms.Button buttonBoltGroup;
+		private System.Windows.Forms.Button buttonCutFitting;
+		private System.Windows.Forms.ListBox listBoxViews;
+		private System.Windows.Forms.Button buttonDeleteView;
+		private System.Windows.Forms.Button buttonCloseView;
+		private System.Windows.Forms.Button buttonOpenView;
+		private System.Windows.Forms.GroupBox groupBoxQuickOnOff;
+		private System.Windows.Forms.GroupBox groupBoxOnOff;
+		private System.Windows.Forms.GroupBox groupBoxViews;
+		private System.Windows.Forms.Button buttonCloseAllViews;
+		private System.Windows.Forms.Button buttonCloseCurrentView;
+		private System.Windows.Forms.Button buttonAllOff;
 	}
 }
 
--- ViewManager/Form1.cs
+++ ViewManager/Form1.cs
@@ -1,20 +1,325 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tekla.Structures.Model.UI;
 
 namespace ViewManager
 {
-	public partial class Form1 : Form
+	public partial class ViewManager : Form
 	{
-		public Form1()
+		private bool _controlLine = true;
+		private bool _controlPoint = true;
+		private bool _baseWeld = true;
+		private bool _baseComponent = true;
+		private bool _part = true;
+		private bool _boltGroup = true;
+		private bool _boolean = true;
+
+		public ViewManager()
 		{
 			InitializeComponent();
+			InitializeUi();
+		}
+
+		private void InitializeUi()
+		{
+			var views = new List<Tekla.Structures.Model.UI.View>();
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				views.Add(currentViews.Current);
+			}
+			listBoxViews.DataSource = views.Select(v => v.Name).ToList();
+		}
+
+		private void buttonOff_Click(object sender, System.EventArgs e)
+		{
+			var picker = new Picker();
+			var modelObject = picker.PickObject(Picker.PickObjectEnum.PICK_ONE_OBJECT);
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+
+				var type = modelObject.GetType().Name.ToUpper();
+				var baseType = modelObject.GetType().BaseType?.Name.ToUpper();
+
+				switch (type)
+				{
+					case "CONTROLLINE":
+						_controlLine = false;
+						currentView.VisibilitySettings.ConstructionLinesVisible = false;
+						currentView.VisibilitySettings.WeldsVisibleInComponents = false;
+						break;
+					case "CONTROLPOINT":
+						_controlPoint = false;
+						currentView.VisibilitySettings.PointsVisible = false;
+						currentView.VisibilitySettings.PointsVisibleInComponents = false;
+						currentView.VisibilitySettings.WeldsVisibleInComponents = false;
+						break;
+				}
+
+				switch (baseType)
+				{
+					case "BASEWELD":
+						_baseWeld = false;
+						currentView.VisibilitySettings.WeldsVisible = false;
+						currentView.VisibilitySettings.WeldsVisibleInComponents = false;
+						break;
+					case "BASECOMPONENT":
+						_baseComponent = false;
+						currentView.VisibilitySettings.ComponentsVisible = false;
+						currentView.VisibilitySettings.ComponentsVisibleInComponents = false;
+						break;
+					case "PART":
+						_part = false;
+						currentView.VisibilitySettings.PartsVisible = false;
+						currentView.VisibilitySettings.PartsVisibleInComponents = false;
+						break;
+					case "BOLTGROUP":
+						_boltGroup = false;
+						currentView.VisibilitySettings.BoltsVisible = false;
+						currentView.VisibilitySettings.BoltsVisibleInComponents = false;
+						currentView.VisibilitySettings.BoltHolesVisible = false;
+						currentView.VisibilitySettings.BoltHolesVisibleInComponents = false;
+						break;
+					case "BOOLEAN":
+						_boolean = false;
+						currentView.VisibilitySettings.CutsVisible = false;
+						currentView.VisibilitySettings.CutsVisibleInComponents = false;
+						currentView.VisibilitySettings.FittingsVisible = false;
+						currentView.VisibilitySettings.FittingsVisibleInComponents = false;
+						break;
+				}
+
+				currentView.Modify();
+			}
+		}
+
+		private void buttonAllOff_Click(object sender, EventArgs e)
+		{
+			OnOff(false);
+		}
+
+		private void buttonOn_Click(object sender, EventArgs e)
+		{
+			OnOff(true);
+		}
+
+		private void OnOff(bool value)
+		{
+			_controlLine = value;
+			_controlPoint = value;
+			_baseWeld = value;
+			_baseComponent = value;
+			_part = value;
+			_boltGroup = value;
+			_boolean = value;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+
+				currentView.VisibilitySettings.ConstructionLinesVisible = value;
+				currentView.VisibilitySettings.WeldsVisibleInComponents = value;
+				currentView.VisibilitySettings.PointsVisible = value;
+				currentView.VisibilitySettings.PointsVisibleInComponents = value;
+				currentView.VisibilitySettings.WeldsVisibleInComponents = value;
+				currentView.VisibilitySettings.WeldsVisible = value;
+				currentView.VisibilitySettings.WeldsVisibleInComponents = value;
+				currentView.VisibilitySettings.ComponentsVisible = value;
+				currentView.VisibilitySettings.ComponentsVisibleInComponents = value;
+				currentView.VisibilitySettings.PartsVisible = value;
+				currentView.VisibilitySettings.PartsVisibleInComponents = value;
+				currentView.VisibilitySettings.BoltsVisible = value;
+				currentView.VisibilitySettings.BoltsVisibleInComponents = value;
+				currentView.VisibilitySettings.BoltHolesVisible = value;
+				currentView.VisibilitySettings.BoltHolesVisibleInComponents = value;
+				currentView.VisibilitySettings.CutsVisible = value;
+				currentView.VisibilitySettings.CutsVisibleInComponents = value;
+				currentView.VisibilitySettings.FittingsVisible = value;
+				currentView.VisibilitySettings.FittingsVisibleInComponents = value;
+
+				currentView.Modify();
+
+			}
+		}
+
+		private void buttonControlLines_Click(object sender, System.EventArgs e)
+		{
+			_controlLine = !_controlLine;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.ConstructionLinesVisible = _controlLine;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonControlPoint_Click(object sender, EventArgs e)
+		{
+			_controlPoint = !_controlPoint;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.PointsVisible = _controlPoint;
+				currentView.VisibilitySettings.PointsVisibleInComponents = _controlPoint;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonBaseWeld_Click(object sender, EventArgs e)
+		{
+			_baseWeld = !_baseWeld;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.WeldsVisible = _baseWeld;
+				currentView.VisibilitySettings.WeldsVisibleInComponents = _baseWeld;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonBaseComponent_Click(object sender, EventArgs e)
+		{
+			_baseComponent = !_baseComponent; 
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.ComponentsVisible = _baseComponent;
+				currentView.VisibilitySettings.ComponentsVisibleInComponents = _baseComponent;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonPart_Click(object sender, EventArgs e)
+		{
+			_part = !_part;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.PartsVisible = _part;
+				currentView.VisibilitySettings.PartsVisibleInComponents = _part;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonBoltGroup_Click(object sender, EventArgs e)
+		{
+			_boltGroup = !_boltGroup;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.BoltsVisible = _boltGroup;
+				currentView.VisibilitySettings.BoltsVisibleInComponents = _boltGroup;
+				currentView.VisibilitySettings.BoltHolesVisible = _boltGroup;
+				currentView.VisibilitySettings.BoltHolesVisibleInComponents = _boltGroup;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonCutFitting_Click(object sender, EventArgs e)
+		{
+			_boolean = !_boolean;
+
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				currentView.Select();
+				currentView.VisibilitySettings.CutsVisible = _boolean;
+				currentView.VisibilitySettings.CutsVisibleInComponents = _boolean;
+				currentView.VisibilitySettings.FittingsVisible = _boolean;
+				currentView.VisibilitySettings.FittingsVisibleInComponents = _boolean;
+				currentView.Modify();
+			}
+		}
+
+		private void buttonOpenView_Click(object sender, EventArgs e)
+		{
+			var viewNames = listBoxViews.SelectedItems.OfType<string>().ToList();
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				if (viewNames.Contains(currentView.Name))
+				{
+					ViewHandler.ShowView(currentView);
+				}
+			}
+		}
+
+		private void buttonCloseView_Click(object sender, EventArgs e)
+		{
+			var viewNames = listBoxViews.SelectedItems.OfType<string>().ToList();
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				if (viewNames.Contains(currentView.Name))
+				{
+					ViewHandler.HideView(currentView);
+				}
+			}
+		}
+
+		private void buttonDeleteView_Click(object sender, EventArgs e)
+		{
+			var viewNames = listBoxViews.SelectedItems.OfType<string>().ToList();
+			var currentViews = ViewHandler.GetAllViews();
+			while (currentViews.MoveNext())
+			{
+				var currentView = currentViews.Current;
+				if (viewNames.Contains(currentView.Name))
+				{
+					currentView.Delete();
+				}
+			}
+
+			InitializeUi();
+		}
+
+		private void buttonCloseSelectedViews_Click(object sender, EventArgs e)
+		{
+			var views = ViewHandler.GetSelectedViews();
+			while (views.MoveNext())
+			{
+				var view = views.Current;
+				ViewHandler.HideView(view);
+			}
+		}
+
+		private void buttonCloseAllViews_Click(object sender, EventArgs e)
+		{
+			var views = ViewHandler.GetAllViews();
+			while (views.MoveNext())
+			{
+				var view = views.Current;
+				ViewHandler.HideView(view);
+			}
 		}
 	}
 }
